9ee975d848c5c56c7d3bd72f371c0a7d291cdc5e
1422041449 <1422041449@qq.com>
2024-11-20 21:18:58 +0800

    提交代码

---
+++ src/main/java/com/learn/learngroup/LearnGroupApplication.java
@@ -0,0 +1,13 @@
+package com.learn.learngroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/learngroup/LearnGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.learngroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
